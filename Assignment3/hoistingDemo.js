6734f71f3abc4ad6e576a1d69d7175a5526e7066
Raj <149650780+raj11gautam@users.noreply.github.com>
2025-11-24 15:48:28 +0530

    Delete Assignment3 directory

--- Assignment3/hoistingDemo.js
+++
@@ -1,9 +0,0 @@
-console.log(score);
-announce();
-var score = 50;
-function announce() { console.log("Game started"); }
-let status = "ready";
-startGame();
-function startGame() {
-console.log(status);
-}
